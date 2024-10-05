d6274b8b5e26c33760c75d40da36038caeed11f0
“Ali <tadayoni.ali.enayat@gmail.com>
2024-10-05 16:44:47 +0330

    interact with DOM elemnt

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ class ProjectInput {
   tempelateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElemet: HTMLInputElement;
+  descriptionInputElemet: HTMLInputElement;
+  peopleInputElemet: HTMLInputElement;
   constructor() {
     this.tempelateElement = document.getElementById(
       "project-input"
@@ -14,8 +17,28 @@ class ProjectInput {
     );
 
     this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = "user-input";
+    this.titleInputElemet = this.element.querySelector(
+      "#title"
+    )! as HTMLInputElement;
+    this.descriptionInputElemet = this.element.querySelector(
+      "#description"
+    )! as HTMLInputElement;
+    this.peopleInputElemet = this.element.querySelector(
+      "#people"
+    )! as HTMLInputElement;
 
+    this.configure();
     this.attach();
+  }
+
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElemet.value);
+  }
+
+  private configure() {
+    this.element.addEventListener("submit", this.submitHandler.bind(this));
   }
 
   private attach() {
