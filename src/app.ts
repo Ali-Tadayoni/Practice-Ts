248309555175a2a872e793b60a7bc808b0423814
“Ali <tadayoni.ali.enayat@gmail.com>
2024-10-05 17:10:54 +0330

    create a reusable autobind decorator

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,18 @@
+// auto bind ddecorator
+
+function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
+
+// ProjectInout class
 class ProjectInput {
   tempelateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
@@ -31,7 +46,7 @@ class ProjectInput {
     this.configure();
     this.attach();
   }
-
+  @autobind
   private submitHandler(event: Event) {
     event.preventDefault();
     console.log(this.titleInputElemet.value);
